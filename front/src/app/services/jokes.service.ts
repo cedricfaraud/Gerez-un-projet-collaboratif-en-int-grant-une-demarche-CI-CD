9352d92cf0cd2cec9dbdcf6858036def4cf0e462
cedricfaraud <cfaraud@gmail.com>
2025-04-03 02:41:18 +0200

    proxy changes
    to test front and back communication

--- front/src/app/services/jokes.service.ts
+++ front/src/app/services/jokes.service.ts
@@ -4,23 +4,25 @@ import { BehaviorSubject, Observable } from 'rxjs';
 import { Joke } from '../model/joke.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class JokesService {
+  private pathService = 'http://localhost:8080/api/joke';
 
-  private pathService = 'api/joke';
-
-  private subject: BehaviorSubject<Joke | null> = new BehaviorSubject<Joke | null>(null);
+  private subject: BehaviorSubject<Joke | null> =
+    new BehaviorSubject<Joke | null>(null);
 
   constructor(private httpClient: HttpClient) {
     this.getRandomJoke();
   }
 
   public getRandomJoke(): void {
-    this.httpClient.get<Joke>(this.pathService).subscribe((joke: Joke) => this.subject.next(joke));
+    this.httpClient
+      .get<Joke>(this.pathService)
+      .subscribe((joke: Joke) => this.subject.next(joke));
   }
 
-  public joke$(): Observable<Joke | null > {
+  public joke$(): Observable<Joke | null> {
     return this.subject.asObservable();
   }
 }
